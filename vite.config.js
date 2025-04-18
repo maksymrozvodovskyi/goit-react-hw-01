c4ec56f0343b3217af92704dcd28740e9a520791
maksym rozvodovskyi <maksym.rozvodovskyi@ukr.net>
2025-04-18 15:50:18 +0300

    edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        exportType: 'default',
+        ref: true,
+        svgo: false,
+        titleProp: true,
+      },
+      include: '**/*.svg',
+    }),
+  ],
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    open: true,
+  },
+});
